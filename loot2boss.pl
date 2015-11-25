b46aba4d3ad000c1821b48923acfceda0a8e3c52
uladz-the1 <vlad.sharanhovich@gmail.com>
2015-11-24 17:35:46 -0800

    Added merging of two LOOT files together - master + user.

--- loot2boss.pl
+++ loot2boss.pl
@@ -1,23 +1,28 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
+#use strict;
+#use warnings;
 use YAML::XS;
 use Getopt::Long;
 use Pod::Usage;
+use Data::Dumper;
 
 ## Tab with # spaces.
 my $opt_loot_file = "";
+my $opt_loot_file2 = "";
 my $opt_boss_file = "";
 my $opt_exec = "";
+my $opt_append = 0;
 my $opt_man = 0;
 my $opt_help = 0;
 
 ## Read and parse the tool command line.
 GetOptions(
-  "loot|l=s" => \$opt_loot_file,
-  "boss|b=s" => \$opt_boss_file,
+  "loot|l|i=s" => \$opt_loot_file,
+  "loot2|l2|i2=s" => \$opt_loot_file2,
+  "boss|output|b|i=s" => \$opt_boss_file,
   "exec|x=s" => \$opt_exec,
+  "append|a" => \$opt_append,
   "help|?" => \$opt_help,
   "man" => \$opt_man)
   or pod2usage(2);
@@ -27,42 +32,90 @@ pod2usage(-exitval => 0, -verbose => 2) if $opt_man;
 ## Check inputs.
 pod2usage("$0: LOOT input file is missing.") if $opt_loot_file eq "";
 
-## Convert LOOT file into BOSS file format.
+## Load master LOOT YAML file.
 my $file = $opt_boss_file;
 if ($file ne "") {
-  print "Converting \"".$opt_loot_file."\"...";
+  print "Converting \"".$opt_loot_file."\"...\n";
 }
 my $ul = YAML::XS::LoadFile($opt_loot_file);
+
+## Load and merge in second LOOT file.
+my $ul2 = ();
+my $replaced_cnt = 0;
+if ($opt_loot_file2 ne "") {
+  print "Merging in \"".$opt_loot_file2."\"...\n";
+  $ul2 = YAML::XS::LoadFile($opt_loot_file2);
+  for (@{$ul2->{plugins}}) {
+    my $name2 = $_->{name};
+    my $tags2 = $_->{tag};
+    next unless defined $tags2;
+    for (@{$ul->{plugins}}) {
+      my $name = $_->{name};
+      if ($name eq $name2) {
+        if ($file ne "") {
+          print "  + user tags \"".$name."\"\n";
+        }
+        $_->{tag} = ();
+        $replaced_cnt++;
+        last;
+      }
+    }
+  }
+}
+
+## Output to a file or console if file is not specified.
 my $f;
 if ($file ne "") {
-  open $f, ">".$file or die $!;
+  open $f, ($opt_append ? ">>" : ">"), $file or die $!;
 }
 else {
   $f = *STDOUT;
 }
-for (@{$ul->{plugins}}) {
+
+## Convert LOOT file into BOSS file format.
+my $input_cnt = 0;
+my $output_cnt = 0;
+for ((@{$ul->{plugins}}, @{$ul2->{plugins}})) {
+  $input_cnt++;
   my $name = $_->{name};
   my $tags = $_->{tag};
-  next unless defined $tags;
+  if (!defined $tags) {
+    $ignored_cnt++;
+    next;
+  }
   print $f "$name"."\n";
   print $f " TAG: {{BASH: ";
   my $ptag = undef;
   for (@{$tags}) {
     print $f $ptag.", " if defined $ptag;
-    $ptag = $_;
+    if (ref($_) eq "HASH") {
+      $ptag = $_->{name};
+    }
+    else {
+      $ptag = $_;
+    }
   }
   print $f $ptag if defined $ptag;
   print $f "}}"."\n";
+  $output_cnt++;
 }
 if ($file ne "") {
   close $f;
-  print " done.\n";
+  print "Output \"".$file."\":\n";
+  print "  written ".$output_cnt." record(s)\n";
+  print "  ignored ".$ignored_cnt." record(s)\n";
+  print "  replaced ".$replaced_cnt." record(s)\n";
 }
 
 ## Run external command after conversion.
 if ($opt_exec ne "") {
-  print "Starting \"".$opt_exec."\"...\n";
+  if ($file ne "") {
+    print "Starting \"".$opt_exec."\"...\n";
+  }
   system($opt_exec);
+}
+if ($file ne "") {
+  print "Done.\n"
 }
 
 __END__
